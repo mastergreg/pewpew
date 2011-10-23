300db3143b73df00f60b0620c88db1ec38ca2364
mastergreg <gregliras@gmail.com>
2011-10-23 21:57:09 +0300

    added sounds

---
+++ sounds.cpp
@@ -0,0 +1,59 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : sounds.cpp
+
+* Purpose :
+
+* Creation Date : 23-10-2011
+
+* Last Modified : Sun 23 Oct 2011 09:56:38 PM EEST
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+
+#include "sounds.h"
+
+sound_player::sound_player()
+{
+  alutInit(NULL,NULL);
+  pew_buffer = alutCreateBufferFromFile("sounds/pew.wav");
+  blast_buffer = pew_buffer = alutCreateBufferFromFile("sounds/blast.wav");
+
+  alGenSources(1,&pew_source);
+  alGenSources(1,&blast_source);
+
+  alSourcei(pew_source,AL_BUFFER,pew_buffer);
+  alSourcei(pew_source,AL_LOOPING,AL_TRUE);
+
+  alSourcei(blast_source,AL_BUFFER,blast_buffer);
+  alSourcei(blast_source,AL_LOOPING,AL_FALSE);
+  sources[PEW]=pew_source;
+  sources[BLAST]=blast_source;
+  if (sounds_on == 1)
+  {
+    alSourcePlay(sources[PEW]);
+  }
+}
+
+void sound_player::play(int sound)
+{
+  if (sounds_on == 1)
+  {
+    alSourcePlay(sources[PEW]);
+    switch(sound)
+    {
+      case BLAST:
+        alSourcePlay(sources[BLAST]);
+        break;
+      default:
+        break;
+    }
+  }
+  else
+  {
+    alSourceStop(sources[PEW]);
+    alSourceStop(sources[BLAST]);
+  }
+}
